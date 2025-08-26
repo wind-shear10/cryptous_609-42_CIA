47aa99f6b06978bf600e72c38d40b06ae6b02899
wind-shear10 <chaaaaaalenkoivan@gmail.com>
2025-08-26 17:40:15 +0300

    улучшения

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ buttonMenu.addEventListener('click', buttonMenuClicked);
 function buttonMenuClicked(){
     mobileNavigation.classList.toggle('opened');
 }
+
+const scrollButton = document.getElementById('scroll-to-top');
+scrollButton.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
